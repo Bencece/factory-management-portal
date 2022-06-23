aec544f95a0e46ba8350c08cc7aff02ff4c54a1d
Bencece <zahoranbence@hotmail.com>
2022-06-23 20:43:54 +0200

    Added auth route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const conn = require("./services/db");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routers:
+//Auth
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+conn.sync();
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server listening on port ${process.env.APP_PORT}`);
