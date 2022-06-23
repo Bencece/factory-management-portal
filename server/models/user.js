24c5ce4274fd4ae3a9d6c18f50913d804b49c62a
Bencece <zahoranbence@hotmail.com>
2022-06-23 20:36:29 +0200

    Created user model

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const conn = require("../db");
+
+const User = conn.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+User.sync();
+
+module.exports = User;
